338606221eb009ff3528efb7042eed2bea564227
Arshak Mkrtchyan <Arshak.Mkrtchyan@acba.am>
2022-05-02 10:40:27 +0400

    initial commit

---
+++ acbacommons/src/main/java/am/acba/acbacommons/EditTextACBA.kt
@@ -0,0 +1,13 @@
+package am.acba.acbacommons
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class EditTextACBA @JvmOverloads constructor(
+    context: Context,
+    attr: AttributeSet? = null,
+    defArrStyle: Int = 0
+) : AppCompatEditText(context, attr, defArrStyle) {
+
+}
